787d6d61762d5895eff7be56b290175fe12015c2
Sadaf khan <GFG19190@gfg.geeksforgeeks.org>
2023-06-11 23:38:10 +0530

    added navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css';
+
+export const Navbar = () =>{
+    return (
+        <div className='container'>
+            <div>logo</div>
+            <div>title</div>
+            <div>searchbox</div>
+            <div>
+                <ul>
+                    <li>wishlist</li>
+                    <li>cart</li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
